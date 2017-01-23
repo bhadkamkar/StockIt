4ba052513f5873c04d51d128eb24eb75548c19d0
Akshay Joshi <akshayaj@usc.edu>
2017-01-22 23:08:22 -0800

    server.js started the server side code for functionality buy

--- server.js
+++ server.js
@@ -4,109 +4,122 @@ var bodyParser = require('body-parser');
 var app = express();
 
 app.set('views', __dirname + '/views');
-app.engine('html',require('ejs').renderFile);
+app.engine('html', require('ejs').renderFile);
 
 app.use(session({secret : "hanabhadjosh"
-}));
+                }));
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended: true}));
 
 var sess;
-
-
 var mysql = require('mysql');
 var connection = mysql.createConnection({
-    host    :'t-02-a.ameyah.com',
-    user    :'stockit1',
-    password:'orchard',
-    database:'stockit'
+    host    : 't-02-a.ameyah.com',
+    user    : 'stockit1',
+    password: 'orchard',
+    database: 'stockit'
 });
 
 connection.connect();
 
-
-app.get('/',function(req,res){
+app.get('/', function (req, res) {
     sess = req.session;
-    if(sess.username){
+    if (sess.username) {
         res.redirect('/dashboard');
-    }
-    else{
+    } else {
         res.render('index.html');
     }
     
 });
 
-app.post('/login',function(req,res){
-    var username = req.body.username;
-    var password = req.body.password;
-    var query = 'SELECT COUNT(*) as rowsCount from users where username = "' + username + '" and password = "' + password + '"';
-    
-    connection.query(query,
-            function(err, rows, fields){
-                if(!err){
-                    if(rows[0].rowsCount == 1){
-                        sess = req.session;
-                        sess.username = username;
-                        res.redirect('/dashboard');    
-                    }
-                    else{
-                        res.redirect('/')
-                    }
-                }
-                else{
-                    console.log(err);
-                }
-    
-    });
-    
-});
-
-app.post('/signup',function(req,res){
-    var username = req.body.username;
-    var password = req.body.password;
-    var email = req.body.email;
-    var query = 'INSERT into users values("' +username + '","' + password + '","' + email +'")';
-    connection.query(query,
-                    function(err,query_res){
-        if(err){
+app.post('/login', function (req, res) {
+    var username = req.body.username, password = req.body.password, query = 'SELECT COUNT(*) as rowsCount from users where username = "' + username + '" and password = "' + password + '"';
+    connection.query(query, function (err, rows, fields) {
+        if (!err) {
+            if (rows[0].rowsCount === 1) {
+                sess = req.session;
+                sess.username = username;
+                res.redirect('/dashboard');
+            } else {
+                res.redirect('/');
+            }
+        } else {
             console.log(err);
         }
-        else{
-            sess = req.session;
-            sess.username = username;
-            res.redirect('/dashboard');    
-        }    
     });
     
 });
 
-app.get('/dashboard', function(req,res){
+
+app.post('/signup', function (req, res) {
+    var username = req.body.username, password = req.body.password, email = req.body.email, query = 'INSERT into users values("' + username + '","' + password + '","' + email + '")';
+    connection.query(query, function (err, query_res) {
+        if (err) {
+            console.log(err);
+        } else {
+            sess = req.session;
+            sess.username = username;
+            res.redirect('/dashboard');
+        }    
+    });
+});
+
+app.get('/dashboard', function (req, res) {
     sess = req.session;
-    if(sess.username){
+    if (sess.username) {
         res.render('dashboard.html');
-        
-    }
-    else{
+    } else {
         res.end('login first');
     }
 });
 
-app.get('/logout',function(req,res){
-    req.session.destroy(function(err){
-        if(err){
+app.get('/logout', function (req, res) {
+    req.session.destroy(function(err) {
+        if (err) {
             console.log(err);
-        }
-        else{
+        } else {
             res.redirect('/');
         }
     });
 });
 
-app.get('*.css|*.js', function(req, res) {
-    var filename = req.url.replace(/^.*[\\\/]/, '')
-    res.sendFile(__dirname + '/views/' + filename);
-})
+app.post('/buy', function (req, res) {
+    //sess = req.session;
+    var username1 = "sid", symbol = "AAPL", rate = 60, count = 2, companyName = "Apple Inc.";
+    var querycheck = 'Select count(*) as rowcount from stocksheld where username = "' + username1 + '" AND symbol = "' + symbol + '"', updateflag = false, insertflag = false;
+    var queryupdate = 'update stocksheld set count = count + ' + count + ', spent = spent + ' + rate * count + 'where username = "' + username1 + '" AND symbol = "' + symbol + '"';
+    var queryinsert = 'insert into stocksheld values ("' + username1 + '","' + symbol + '","' + companyName + '",' + count + ',' + (count * rate) + ')';
+    connection.query(querycheck, function (err, rows, fields) {
+        if (!err) {
+            if (rows[0].rowcount === 1) {
+                updateflag = true;
+            } else if (rows[0].rowcount === 0) {
+                insertflag = true;
+            }
+        } else {
+            console.log(err);
+        }
+    });
+    if (updateflag) {
+        connection.query(queryupdate, function (err, rows, fields) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    } else if(insertflag) {
+        connection.query(queryinsert, function (err, rows, fields) {
+            if (err) {
+                console.log(err);
+            }
+        });
+    }
+});
 
-app.listen(4000,function(){
+app.get('*.css|*.js', function (req, res) {
+    var filename = req.url.replace(/^.*[\\\/]/, '');
+    res.sendFile(__dirname + '/views/' + filename);
+});
+
+app.listen(4001, function () {
     console.log('started on port 4000');
 });
